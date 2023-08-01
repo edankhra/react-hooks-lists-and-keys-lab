02868de6dd909b1d4b067b42c45d8a97806da49e
Ekta <edankhra@gmail.com>
2023-08-01 08:47:48 -0400

    completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">{/* render ProjectItem components here */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+const ProjectList = ({ projects }) => {
   return (
-    <div id="projects">
+    <div>
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          name={project.name}
+          description={project.description}
+          url={project.url}
+          technologies={project.technologies} // Pass the technologies array as a prop
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default ProjectList;
